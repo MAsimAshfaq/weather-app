b2baa120b81739f9f10dacc315f53064a80c4848
iftikhar708 <iftikhar.ahmad@purelogics.net>
2023-12-14 18:20:39 +0500

    create home page

---
+++ app/location/[city]/[lat]/[long]/page.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+type Props = {
+  params: {
+    city: string;
+    lat: string;
+    long: string;
+  };
+};
+function WeatherPage({ params: { city, lat, long } }: Props) {
+  return (
+    <div>
+      Welcome to the Weather Page: {city} {lat} {long}
+    </div>
+  );
+}
+
+export default WeatherPage;
